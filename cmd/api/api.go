f090a0d7686a3b30233ca3318997cd3c59aa0e11
Marcos Cavalcante <11593806+marcosmcb@users.noreply.github.com>
2024-12-25 19:28:48 +0000

    docs: add docs implementation

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/marcosmcb/backend-engineering-with-go/docs" // This is required to generate the Swagger docs
 	"github.com/marcosmcb/backend-engineering-with-go/internal/store"
+	httpSwagger "github.com/swaggo/http-swagger/v2" // http-swagger middleware
 )
 
 type application struct {
@@ -16,9 +19,10 @@ type application struct {
 }
 
 type config struct {
-	addr string
-	db   dbConfig
-	env  string
+	addr   string
+	db     dbConfig
+	env    string
+	apiURL string
 }
 
 type dbConfig struct {
@@ -45,6 +49,9 @@ func (app *application) mount() http.Handler {
 	r.Get("/health", app.healthCheckHandler)
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+
+		docsURL := fmt.Sprintf("%s/swagger/doc.json", app.config.addr)
+		r.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL(docsURL)))
 
 		// POST /v1/posts
 		r.Route("/posts", func(r chi.Router) {
@@ -76,6 +83,11 @@ func (app *application) mount() http.Handler {
 }
 
 func (app *application) run(mux http.Handler) error {
+	// Docs
+	docs.SwaggerInfo.Version = version
+	docs.SwaggerInfo.Host = app.config.apiURL
+	// docs.SwaggerInfo.BasePath = "/v1"
+
 	srv := &http.Server{
 		Addr:         app.config.addr,
 		Handler:      mux,
